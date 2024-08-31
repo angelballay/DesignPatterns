cac869e100916f3ed0ad4116cdbcb5552506c567
angelballay <angel.ballay.dev@gmail.com>
2024-08-31 15:40:15 -0300

    Se añaden los cambios del patron estructural fachada

---
+++ Estructural.Facade/VideoConverterFacade.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace Estructural.Facade
+{
+    /// <summary>
+    /// Se crea la clase fachara para esconder la complejdiad del framework tras una interfaz simple.
+    /// Es una solucon de equilibrio entre funcionalidad y simplicidad.
+    /// </summary>
+    public class VideoConverterFacade
+    {
+        public Estructural.Facade.Framework.File Convert(string filename, string format)
+        {
+            var file = new Estructural.Facade.Framework.VideoFile();
+            var destinationCode = new Estructural.Facade.Framework.CodecFactory();
+            var sourceCode = new object();
+            if(format == "mp4")
+            {
+                sourceCode = new Estructural.Facade.Framework.MPEG4CompressionCodec();
+            }else
+            {
+                sourceCode = new Estructural.Facade.Framework.OggCompressionCodec();
+            }
+
+            var result = new Estructural.Facade.Framework.AudioMixer(file, destinationCode,sourceCode);
+
+            return new Estructural.Facade.Framework.File(filename,format);
+
+        }
+    }
+}
